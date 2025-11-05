b858f8d68fd4f4b7d98b1ff2037b6eed3db79369
Karim Smires <k.smires101@icloud.com>
2025-11-05 11:28:54 -0500

    Initial Commit

---
+++ DemoUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoUI
+//
+//  Created by Karim Smires on 11/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoUI/DemoUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoUIApp.swift
+//  DemoUI
+//
+//  Created by Karim Smires on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
